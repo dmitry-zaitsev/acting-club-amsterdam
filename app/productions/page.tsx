e3aef41fa2bb7dcb971a811b5e47a09d18dc953c
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2023-06-08 00:45:18 +0200

    Added Logo to Productions page

--- app/productions/page.tsx
+++ app/productions/page.tsx
@@ -5,11 +5,9 @@ import { ProductionsList } from "@/src/components/ProductionsList"
 export default function Productions() {
     return (
         <main className="flex min-h-screen flex-col items-center pt-1 pb-4 gap-3">
-            <Navbar />
+            <Navbar showLogo={true} />
 
             <ProductionsList />
-
-            <Footer />
         </main>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,12 @@ import { CSSProperties } from "react"
 import { bebas } from "../style/fonts"
 import { tailwindStyles } from "../style/styles"
 import Link from "next/link"
+import Image from "next/image"
+
+
+type NavbarProps = {
+    showLogo?: boolean;
+}
 
 const styles: { [key: string]: CSSProperties } = {
     container: {
@@ -10,9 +16,22 @@ const styles: { [key: string]: CSSProperties } = {
     }
 }
 
-export const Navbar = () => {
+export const Navbar = ({ showLogo } : NavbarProps) => {
     return (
-        <div className={`flex flex-row gap-4 justify-end items-center ${tailwindStyles.pageComponent} ${bebas.className}`} style={styles.container}>
+        <div className={`flex flex-row gap-4 items-center ${tailwindStyles.pageComponent} ${bebas.className}`} style={styles.container}>
+            {showLogo && (
+                <Link href="/">
+                    <Image
+                        src="Logo.svg"
+                        alt="Acting Club Amsterdam"
+                        width={72}
+                        height={37}
+                    />
+                </Link>
+            )}
+
+            <div className="flex-grow" />
+
             <Link href="/productions">
                 Productions
             </Link>
